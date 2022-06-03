da446b9552f458cd1e415ebcc306244de2c278c8
Vineet Gupta <vineet091@users.noreply.github.com>
2022-06-03 16:52:40 +0530

    promise all

--- src/index.js
+++ src/index.js
@@ -1,65 +1,65 @@
 // import "./styles.css";
-// var urls = [
-//   "https://api.github.com/users/iliakan",
-//   "https://remy.com",
-//   "https://api.github.com/users/jeresig"
-// ];
+var urls = [
+  "https://api.github.com/users/iliakan",
+  "https://remy.com",
+  "https://api.github.com/users/jeresig"
+];
 
 // // Each Promise handle error itself then promise all will return arry of all promises output
-// Promise.all(
-//   urls.map((url, index) => {
-//     if (index % 2 !== 0) {
-//       return new Promise((resolve, reject) => {
-//         resolve(index);
-//       });
-//     } else {
-//       return new Promise((resolve, reject) => {
-//         reject("Failed to Fetch");
-//       }).then(
-//         (response) => {
-//           return response;
-//         },
-//         (err) => {
-//           return err;
-//         }
-//       );
-//     }
-//   })
-// ).then((responses) => {
-//   console.log(responses);
-// });
+Promise.all(
+  urls.map((url, index) => {
+    if (index % 2 !== 0) {
+      return new Promise((resolve, reject) => {
+        resolve(index);
+      });
+    } else {
+      return new Promise((resolve, reject) => {
+        reject("Failed to Fetch");
+      }).then(
+        (response) => {
+          return response;
+        },
+        (err) => {
+          return err;
+        }
+      );
+    }
+  })
+).then((responses) => {
+  console.log(responses);
+});
 
 // // Each Promise not handled error itself then Promise all will return failed promise result and stop exeuting further promises
-// Promise.all(
-//   urls.map((url, index) => {
-//     if (index % 2 !== 0) {
-//       return new Promise((resolve, reject) => {
-//         resolve(index);
-//       });
-//     } else {
-//       return new Promise((resolve, reject) => {
-//         reject("Failed to Fetch");
-//       });
-//     }
-//   })
-// )
-//   .then((responses) => {
-//     console.log(responses);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+Promise.all(
+  urls.map((url, index) => {
+    if (index % 2 !== 0) {
+      return new Promise((resolve, reject) => {
+        resolve(index);
+      });
+    } else {
+      return new Promise((resolve, reject) => {
+        reject("Failed to Fetch");
+      });
+    }
+  })
+)
+  .then((responses) => {
+    console.log(responses);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // // Each Promise handled error itself then Promise all will return array of promises result and we can filter out error by using filter Boolean.
-// Promise.all(
-//   urls.map((url) =>
-//     fetch(url)
-//       .then((r) => r.json())
-//       .catch((e) => "")
-//   )
-// ).then((responses) => {
-//   console.log(responses.filter(Boolean));
-// });
+Promise.all(
+  urls.map((url) =>
+    fetch(url)
+      .then((r) => r.json())
+      .catch((e) => "")
+  )
+).then((responses) => {
+  console.log(responses.filter(Boolean));
+});
 
 // Call array of promise synchronously
 
@@ -187,22 +187,22 @@ executePromises(promises);
 
 //Promise Pollyfill
 function myPromise(func) {
-  var resolve;
-  var reject;
+  var onResolve;
+  var onRreject;
   function exec(val) {
-    resolve(val);
+    onResolve(val);
   }
 
   function exec2(val) {
-    reject(val);
+    onRreject(val);
   }
   this.then = function (callback) {
-    resolve = callback;
+    onResolve = callback;
     return this;
   };
 
   this.catch = function (callback) {
-    reject = callback;
+    onRreject = callback;
     return this;
   };
 
@@ -217,3 +217,42 @@ new myPromise((resolve, reject) => {
     console.log(response);
   })
   .catch((err) => console.log(err));
+
+function myPromiseAll(promises) {
+  return new myPromise((res, rej) => {
+    var count = promises.length;
+    var result = [];
+    promises.map((promise1) => {
+      promise1
+        .then((payload) => {
+          result.push(payload);
+          count--;
+          if (count === 0) {
+            res(result);
+          }
+        })
+        .catch((err) => {
+          rej(err);
+        });
+    });
+  });
+}
+
+myPromiseAll([
+  new myPromise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(true);
+    }, 1000);
+  }),
+  new myPromise((resolve, reject) => {
+    setTimeout(() => {
+      reject(false);
+    }, 1000);
+  })
+])
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
